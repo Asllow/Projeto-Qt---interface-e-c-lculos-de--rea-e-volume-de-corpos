910c81da712c163edbe6422275208554db7dbf90
GuiALO <guilhermenbr@gmail.com>
2022-11-05 18:31:41 -0300

    send

--- CPonto.py
+++ CPonto.py
@@ -1,3 +1,4 @@
+import numpy as np
 class CPonto:
     def __init__ (self, x=0, y=0, r=0, ang=0):
         self.x = x
@@ -6,6 +7,9 @@ class CPonto:
         self.ang = ang
     def getCoordCard(self):
         return self.x, self.y
-    def setPonto(self, x=0, y=0):
+    def setCoordCart(self, x=0, y=0):
         self.x = x
         self.y = y
+    def conversor(self):
+        self.r = (self.x**2+self.y**2)**(0.5)
+        self.ang = np.arctan(self.y/self.x)
