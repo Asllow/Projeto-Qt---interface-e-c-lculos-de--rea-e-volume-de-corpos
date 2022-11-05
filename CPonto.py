59454364c99d3b55684ec4ecbd0062be5a16028b
GuiALO <guilhermenbr@gmail.com>
2022-11-05 18:45:47 -0300

    send

--- CPonto.py
+++ CPonto.py
@@ -5,14 +5,19 @@ class CPonto:
         self.y = y
         self.r = r
         self.ang = ang
-    def getCoordCard(self):
-        return self.x, self.y
-    def setCoordCart(self, x=0, y=0):
-        self.x = x
-        self.y = y
     def conversor(self):
         self.r = (self.x**2+self.y**2)**(0.5)
         self.ang = np.degrees(np.arctan(self.y/self.x))
+    def getCoordCart(self):
+        return self.x, self.y
+    def getCoordPol(self):
+        return self.r, self.ang
+    def setCoordCart(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    def setCoordPol(self, r=0, ang=0):
+        self.r = r
+        self.ang = ang
 
 y = CPonto(1, 1)
 y.conversor()
