9e892c974e65d4508865f0a6279892e53b78ea51
Asllow <arthurmir0429@gmail.com>
2022-11-05 18:15:37 -0300

    Pull Request

--- CPonto.py
+++ CPonto.py
@@ -2,7 +2,7 @@ class CPonto:
     def __init__ (self, x=0, y=0):
         self.x = x
         self.y = y
-    def getCoordenadas(self):
+    def getCoordCard(self):
         return self.x, self.y
     def setPonto(self, x=0, y=0):
         self.x = x
