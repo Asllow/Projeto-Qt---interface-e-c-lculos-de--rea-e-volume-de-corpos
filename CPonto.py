9d5807787bd9e9a6d30442baf80b69a64a640f51
Asllow <arthurmir0429@gmail.com>
2022-11-09 14:17:11 -0300

    teste

--- CPonto.py
+++ CPonto.py
@@ -1,11 +1,9 @@
 import numpy as np
 
 class CPonto:
-    def __init__ (self, x=0, y=0, r=0, ang=0):
+    def __init__ (self, x=0, y=0):
         self.x = x
         self.y = y
-        self.r = r
-        self.ang = np.radians(ang)
     def getCoordenates(self):
         return self.x, self.y
     def setCoordenates(self, x=0, y=0):
