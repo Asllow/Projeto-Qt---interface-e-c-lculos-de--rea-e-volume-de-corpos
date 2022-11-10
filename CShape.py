d4111ef0f1425637b2d78afe38adb0a46ee21aa8
Asllow <arthurmir0429@gmail.com>
2022-11-10 11:31:25 -0300

    teste

--- CShape.py
+++ CShape.py
@@ -62,17 +62,57 @@ class CShape:
         x2, y2 = p2.getCoordinates()
         x3, y3 = p3.getCoordinates()
         x4, y4 = p4.getCoordinates()
-        s1 = x1*y2 + x2*y3 + x3*y4 + x4*y1
-        s2 = y1*x2 + y2*x3 + y3*x4 + y4*x1
-        a = abs((s1 - s2)/2)
-        return a
+        listpoints = [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
+        listpoints.sort()
+        
+        M = np.ones((3, 3), dtype= float)
+        N = np.ones((3, 3), dtype= float) 
+        
+        M[0, 0] = listpoints[0][0]
+        M[0 , 1] = listpoints[0][1]
+        M[1 , 0] = listpoints[1][0]
+        M[1 , 1] = listpoints[1][1]
+        M[2 , 0] = listpoints[2][0]
+        M[2 , 1] = listpoints[2][1]
+        D= (abs(np.linalg.det(M)))/2
+        
+        N[0, 0] = listpoints[3][0]
+        N[0 , 1] = listpoints[3][1]
+        N[1 , 0] = listpoints[1][0]
+        N[1 , 1] = listpoints[1][1]
+        N[2 , 0] = listpoints[2][0]
+        N[2 , 1] = listpoints[2][1]
+        P= (abs(np.linalg.det(N)))/2
+        
+        area = abs(D+P)
+        return area
     @dispatch(CPonto, CPonto, CPonto, CPonto)
     def areaPolygon(p1, p2, p3, p4):
         x1, y1 = p1.getCoordinates()
         x2, y2 = p2.getCoordinates()
         x3, y3 = p3.getCoordinates()
         x4, y4 = p4.getCoordinates()
-        s1 = x1*y2 + x2*y3 + x3*y4 + x4*y1
-        s2 = y1*x2 + y2*x3 + y3*x4 + y4*x1
-        a = abs((s1 - s2)/2)
-        return a
+        listpoints = [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
+        listpoints.sort()
+        
+        M = np.ones((3, 3), dtype= float)
+        N = np.ones((3, 3), dtype= float) 
+        
+        M[0, 0] = listpoints[0][0]
+        M[0 , 1] = listpoints[0][1]
+        M[1 , 0] = listpoints[1][0]
+        M[1 , 1] = listpoints[1][1]
+        M[2 , 0] = listpoints[2][0]
+        M[2 , 1] = listpoints[2][1]
+        D= (abs(np.linalg.det(M)))/2
+        
+        N[0, 0] = listpoints[3][0]
+        N[0 , 1] = listpoints[3][1]
+        N[1 , 0] = listpoints[1][0]
+        N[1 , 1] = listpoints[1][1]
+        N[2 , 0] = listpoints[2][0]
+        N[2 , 1] = listpoints[2][1]
+        P= (abs(np.linalg.det(N)))/2
+        
+        area = abs(D+P)
+        return area
