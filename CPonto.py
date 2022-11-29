241ebca389aabd0bf14ea7119067402a2bd6b09f
Asllow <arthurmir0429@gmail.com>
2022-11-29 13:13:17 -0300

    pull

--- CPonto.py
+++ CPonto.py
@@ -1,11 +1,13 @@
 class CPonto:
-    def __init__ (self, x=0, y=0):
+    def __init__ (self, x=0, y=0, z=0):
         self.x = x
         self.y = y
+        self.z = z
     def getCoordinates(self):
-        return self.x, self.y
-    def setCoordinates(self, x=0, y=0):
+        return self.x, self.y, self.z
+    def setCoordinates(self, x=0, y=0, z=0):
         self.x = x
         self.y = y
+        self.z = z
     def onlyPoint(*args):
         return list(args)
--- View.py
+++ View.py
@@ -4,7 +4,8 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5  import QtCore, QtGui, QtWidgets
 from PyQt5.uic import loadUiType
 from CShape import *
-from scipy.spatial import ConvexHull, convex_hull_plot_2d
+from scipy.spatial import ConvexHull
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 Ui_MainWindow, QMainWindow = loadUiType("form.ui")
@@ -64,19 +65,27 @@ class View (QMainWindow, Ui_MainWindow):
         self.result.setText(result)
 if __name__ == '__main__':
     rng = np.random.default_rng()
-    points = np.array([[0, 0, 0],
-                       [10, 0, 0],
-                       [10, 10, 0],
-                       [0, 10, 0],
-                       [0, 0, 2],
-                       [10, 0, 2],
-                       [10, 10, 2],
-                       [0, 10, 2]])
+    point = np.array([[0,0,0],[10, 0, 0],[10, 10, 0],[0, 10, 0],[0, 0, 2],[10, 0, 2],[10, 10, 2],[0, 10, 2]])
+    point = np.array([[0, 0],[10, 0],[10, 10],[0, 10]])
+    points = np.array([[0,0,0],[3,0,0],[0,3,0],[3,3,0],[1.5,1.5,6]])
     hull = ConvexHull(points)
     print(hull.volume)
+    #2D
     plt.plot(points[:,0], points[:,1], 'o')
     for simplex in hull.simplices:
         plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
+        
     plt.plot(points[hull.vertices,0], points[hull.vertices,1], 'r--', lw=2)
     plt.plot(points[hull.vertices[0],0], points[hull.vertices[0],1], 'ro')
+    plt.show()
+    #3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot(points.T[0], points.T[1], points.T[2], "ko")
+    
+    for s in hull.simplices:
+        s = np.append(s, s[0])  # Here we cycle back to the first coordinate
+        ax.plot(points[s, 0], points[s, 1], points[s, 2], "r-")
+    for i in ["x", "y", "z"]:
+        eval("ax.set_{:s}label('{:s}')".format(i, i))
     plt.show()
