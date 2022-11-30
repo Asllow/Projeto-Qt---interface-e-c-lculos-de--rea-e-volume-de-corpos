0e118c4985b9d19c3f3613c9297bfff7b09510b9
Asllow <arthurmir0429@gmail.com>
2022-11-29 21:10:11 -0300

    teste

--- View.py
+++ View.py
@@ -1,24 +1,31 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QPixmap
 from PyQt5  import QtCore, QtGui, QtWidgets
 from PyQt5.uic import loadUiType
 from CShape import *
 from scipy.spatial import ConvexHull
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from PIL import Image
 
-Ui_MainWindow, QMainWindow = loadUiType("form.ui")
+Ui_MainWindow, QMainWindow = loadUiType("formulario.ui")
 
 class View (QMainWindow, Ui_MainWindow):
     def __init__ (self):
         super(View, self).__init__()
         self.setupUi(self)
-        self.setWindowTitle("Cálculo de Área")
-        
-        self.area_circle.clicked.connect(self.on_button_clicked_circle)
-        self.area_triangle.clicked.connect(self.on_button_clicked_triangle)
-        self.area_polygon.clicked.connect(self.on_button_clicked_polygon)
+        self.setWindowTitle("Cálculo de Área e Volume")
+        self.listapontos = []
+        #self.area.clicked.connect(self.on_button_clicked_circle)
+        self.area.clicked.connect(self.on_button_clicked_area)
+        self.adicionar.clicked.connect(self.on_add_point)
+        self.limpar.clicked.connect(self.on_limpar)
+    def on_limpar (self):
+        self.quadro.clear()
+        self.listapontos.clear()
+        print (self.listapontos)
     def on_button_clicked_circle (self):
         x1 = float(self.x1.text())
         y1 = float(self.y1.text())
@@ -33,36 +40,15 @@ class View (QMainWindow, Ui_MainWindow):
             area = CShape.areaCircle(p1, p2)
         result = str(area)
         self.result.setText(result)
-    def on_button_clicked_triangle (self):
-        x1 = float(self.x1.text())
-        x2 = float(self.x2.text())
-        x3 = float(self.x3.text())
-        y1 = float(self.y1.text())
-        y2 = float(self.y2.text())
-        y3 = float(self.y3.text())
-        p1 = CPonto(x1, y1)
-        p2 = CPonto(x2, y2)
-        p3 = CPonto(x3, y3)
-        area = CShape.area(p1, p2, p3)
-        
-        result = str(area)
-        self.result.setText(result)
-    def on_button_clicked_polygon (self):
-        x1 = float(self.x1.text())
-        x2 = float(self.x2.text())
-        x3 = float(self.x3.text())
-        x4 = float(self.x4.text())
-        y1 = float(self.y1.text())
-        y2 = float(self.y2.text())
-        y3 = float(self.y3.text())
-        y4 = float(self.y4.text())
-        
-        x = CPonto.onlyPoint(x1, x2, x3, x4)
-        y = CPonto.onlyPoint(y1, y2, y3, y4)
-        
-        area = CShape.area(x, y)
-        result = str(area)
-        self.result.setText(result)
+    def on_add_point (self):
+        x = float(self.x.text())
+        y = float(self.y.text())
+        z = float(self.z.text())
+        self.quadro.appendPlainText(f"[{x}, {y}, {z}]")
+        self.listapontos.append([x, y, z])
+    def on_button_clicked_area (self):
+        self.pixmap = QPixmap('teste.png')
+        self.image.setPixmap(self.pixmap)
 if __name__ == '__main__':
     rng = np.random.default_rng()
     point = np.array([[0,0,0],[10, 0, 0],[10, 10, 0],[0, 10, 0],[0, 0, 2],[10, 0, 2],[10, 10, 2],[0, 10, 2]])
@@ -77,6 +63,10 @@ if __name__ == '__main__':
         
     plt.plot(points[hull.vertices,0], points[hull.vertices,1], 'r--', lw=2)
     plt.plot(points[hull.vertices[0],0], points[hull.vertices[0],1], 'ro')
+    plt.savefig('teste.png', format='png')
+    image = Image.open('teste.png')
+    new_image = image.resize((241, 221))
+    new_image.save('teste.png')
     plt.show()
     #3D
     fig = plt.figure()
@@ -88,4 +78,8 @@ if __name__ == '__main__':
         ax.plot(points[s, 0], points[s, 1], points[s, 2], "r-")
     for i in ["x", "y", "z"]:
         eval("ax.set_{:s}label('{:s}')".format(i, i))
+    plt.savefig('teste.png', format='png')
+    image = Image.open('teste.png')
+    new_image = image.resize((241, 221))
+    new_image.save('teste.png')
     plt.show()
